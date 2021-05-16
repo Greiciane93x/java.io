4e48536e926442b2fcdf7775a20d7ca7442152a6
Ane <greiciane_araujo@hotmail.com>
2021-05-16 14:29:35 -0300

    java io

---
+++ java-io/src/br/studant/java/io/test/TesteLeitura.java
@@ -0,0 +1,30 @@
+package br.studant.java.io.test;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TesteLeitura {
+
+	public static void main(String[] args) throws IOException {
+		
+		// Fluxo de Entrada com Arquivo
+		FileInputStream fis = new FileInputStream("texto.txt");
+		InputStreamReader isr = new InputStreamReader(fis); 
+		BufferedReader br = new BufferedReader(isr); 
+		
+		String linha = br.readLine(); 
+		
+		while(linha != null) {
+			System.out.println(linha);
+			linha = br.readLine(); 
+		}
+		
+		System.out.println(linha);
+		
+		br.close();
+		
+	}
+
+}
